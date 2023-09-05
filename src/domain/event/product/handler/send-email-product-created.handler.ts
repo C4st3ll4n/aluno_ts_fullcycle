72fa47cfaa78a0f8b22e909583c94aac563bba2b
Pedro Henrique <p13dr0h@gmail.com>
2023-09-05 18:38:29 -0300

    feat: add product created event implementation

---
+++ src/domain/event/product/handler/send-email-product-created.handler.ts
@@ -0,0 +1,15 @@
+import EventHandlerInterface from "../../shared/event-handler.interface";
+import ProductCreatedEvent from "../product-created.event";
+
+export class SendEmailWhenProductCreatedHandler implements  EventHandlerInterface<ProductCreatedEvent>{
+    handle(event: ProductCreatedEvent): void {
+        // tslint:disable:no-console
+        console.log(`Sending email to ${event.eventData.email}...`)
+        const _success = Math.random()
+        if(_success===1){
+            console.log("Email sent successfully")
+        }else{
+            console.log("Fail to send email")
+        }
+    }
+}
