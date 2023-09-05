87c69a1c0351bfd25b161bc536789e8bb53228bd
Pedro Henrique <p13dr0h@gmail.com>
2023-09-05 15:59:03 -0300

    feat: add order repo implementations

--- src/infra/repository/order.repository.ts
+++ src/infra/repository/order.repository.ts
@@ -2,6 +2,9 @@ import OrderRepositoryInterface from "../../domain/repository/order-repository.i
 import Order from "../../domain/entity/order";
 import OrderModel from "../db/sequelize/model/order.model";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
+import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
+import OrderItem from "../../domain/entity/order_item";
 
 export class OrderRepository implements  OrderRepositoryInterface {
     async create(entity: Order): Promise<void> {
@@ -22,15 +25,41 @@ export class OrderRepository implements  OrderRepositoryInterface {
     }
 
     async find(id: string): Promise<Order> {
-        return Promise.resolve(undefined);
+        const orderModel = await OrderModel.findOne(
+            {where: {id}}
+        )
+
+        return new Order(orderModel.id, orderModel.customerId, orderModel.items.map((item): OrderItem => {
+            return new OrderItem(item.id, item.name, item.price, item.quantity, item.productId);
+        }));
     }
 
     async findAll(): Promise<Order[]> {
-        return Promise.resolve([]);
+        const orderModels = await OrderModel.findAll();
+        return orderModels.map((orderModel): Order => {
+            return new Order(orderModel.id, orderModel.customerId, orderModel.items.map((item): OrderItem => {
+                return new OrderItem(item.id, item.name, item.price, item.quantity, item.productId);
+            }));
+        })
     }
 
     async update(entity: Order): Promise<void> {
-        return Promise.resolve(undefined);
+        await OrderModel.update({
+            id: entity.id(),
+            customerId: entity.customer(),
+            total: entity.total(),
+            items: entity.items().map((item) => ({
+                id: item.id(),
+                name: item.name(),
+                price: item.price,
+                productId: item.product(),
+                quantity: item.quantity()
+            }))
+        }, {
+            where: {
+                id: entity.id()
+            }
+        })
     }
 
 }
